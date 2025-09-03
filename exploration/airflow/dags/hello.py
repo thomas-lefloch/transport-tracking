813f6c978282c0270d4efa68af57dbd7c75ef6c9
thomas-lefloch <t.lefloch53@gmail.com>
2025-09-03 12:07:39 +0200

    fix: dag id

--- exploration/airflow/dags/hello.py
+++ exploration/airflow/dags/hello.py
@@ -4,7 +4,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 from airflow.sdk import DAG, task
 
 # A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
+with DAG(dag_id="hello", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
     # Tasks are represented as operators
     hello = BashOperator(task_id="hello", bash_command="echo hellooifjdsfoijeo")
 
